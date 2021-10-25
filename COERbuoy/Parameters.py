dc2b75f98a8b9513033b424b3244e800c45fd158
Simon H. Thomas <simon.thomas.2021@mumail.ie>
2021-10-25 23:46:24 +0100

    utils: setting are first read from package then overwritten by local settings; controller are read from different localtions; WECpath fct; Parameters: class parameters for dynamic controller parametrisation

--- COERbuoy/Parameters.py
+++ COERbuoy/Parameters.py
@@ -13,65 +13,126 @@ from COERbuoy import floater_LIN as Floater;
 from COERbuoy import wavefield;
 import importlib;
 import os;
+import json;
  
 #import dynamics_sphere as dynamics
+
+class parameters():
+    wec=None;
+    cut_off=100;
+    classhydro=None;
+    pto_mdc=[0,0,0];
+    floater=None;
+    params=None;
+    omega=None;
+    def __init__ (self,wecdir=None,classhydro=None):
+        if wecdir==None or classhydro==None:
+            utils.get();
+        if wecdir==None:
+            wecdir=utils.wec_dir;
+        if classhydro==None:
+            classhydro=utils.class_hydro;
+        spec=importlib.util.spec_from_file_location("dynamics.py",os.path.join(utils.WECpath(wecdir),"dynamics.py"));
+        dynamics=importlib.util.module_from_spec(spec);
+        spec.loader.exec_module(dynamics);
+        self.wec=dynamics.WEC();
+        self.cut_off=self.wec.omega_cut_off;
+        self.classhydro=classhydro;
+    
+    def init_hydro(self,omega):
+        g=9.81;
+        self.omega=omega;
+        xi=omega*omega/g;
+        #Initialize WEC
+        self.wec.load_buoy(getattr(Floater,utils.class_hydro),xi,300,0);
+        self.wec.load_param();
+        self.floater=self.wec.buoy;
+        
+    def area_vol (self,z):
+        area=self.floater.Area(z);
+        vol=self.floater.Volume(z);
+        return [area,vol];
+    
+    def hydro (self,z,mode1,mode2):
+        results=self.floater.Calculate(z,0,0,0)
+        Fstat=results[0][mode1];
+        Fexc=results[1][mode1];
+        Zrad=results[2][mode1][mode2];
+        Am=results[3][mode1];
+        return [Fstat,Fexc,Zrad,Am];
+    
+    def pto_mdc (self,z):
+        return self.wec.pto_mdc(z);
+    
+    def eff_generator(self,v,f):
+            wave=wavefield.wavefield(self.omega*0,self.omega*0,self.omega);
+            x=np.zeros(self.wec.states);
+            x[1]=v;
+            return self.wec.Calc(0,wave,x,-1*f,0,[1,1,1])[8]/(f*v);
+    
+    def dic_param(self):
+        with open(self.wec.file) as file:
+            return json.load(file);
+        
+        
 def run():
-    utils.get();
+    wec=parameters();
+    #utils.get();
     
-    spec=importlib.util.spec_from_file_location("dynamics.py",os.path.join(utils.wec_dir,"dynamics.py"));
-    dynamics=importlib.util.module_from_spec(spec);
-    spec.loader.exec_module(dynamics);
-    wec=dynamics.WEC();
-    omega=np.round(np.linspace(0.1,wec.omega_cut_off,10),10);#select frequency used
-    xi=omega*omega/9.81;#
+    #spec=importlib.util.spec_from_file_location("dynamics.py",os.path.join(utils.wec_dir,"dynamics.py"));
+    #dynamics=importlib.util.module_from_spec(spec);
+    #spec.loader.exec_module(dynamics);
+    #wec=dynamics.WEC();
+    omega=np.round(np.linspace(0.1,wec.cut_off,10),10);#select frequency used
+    #xi=omega*omega/9.81;#
     
     
-    #Initialize WEC
-    wec.load_buoy(getattr(Floater,utils.class_hydro),xi,300,0);
-    wec.load_param();
+    ##Initialize WEC
+    #wec.load_buoy(getattr(Floater,utils.class_hydro),xi,300,0);
+    #wec.load_param();
+    wec.init_hydro(omega);
     
     #limit=7;#heave limit inbetween which to calculate parameters
     delta=0.1;
-    geoBox=wec.buoy.getGeoBox();#get limits inbetween which to calculate parameters
+    geoBox=wec.floater.getGeoBox();#get limits inbetween which to calculate parameters
     zs=np.linspace(geoBox[0],geoBox[1],18+1);#submergence levels at which to evaluate
     n_mode=3;
     area=np.zeros(zs.size);
     vol=np.zeros(zs.size);
     Fstat=np.copy([np.zeros(zs.size)]*n_mode);
-    Fdyn=np.copy([np.zeros([zs.size,xi.size])]*n_mode);
-    Frad=np.copy([np.zeros([zs.size,xi.size])]*n_mode);
-    Amass=np.copy([np.zeros([zs.size,xi.size])]*n_mode);
-    Fdiff=np.copy([np.zeros([zs.size,xi.size])]*n_mode);
+    Fdyn=np.copy([np.zeros([zs.size,omega.size])]*n_mode);
+    Frad=np.copy([np.zeros([zs.size,omega.size])]*n_mode);
+    Amass=np.copy([np.zeros([zs.size,omega.size])]*n_mode);
+    Fdiff=np.copy([np.zeros([zs.size,omega.size])]*n_mode);
     Am8=np.copy([np.zeros([zs.size])]*n_mode);
     floater_slider_spring=np.zeros(zs.size);
-    
     #Madd-Damper-Spring parameters
     mdc=np.array([0,0,0]);
     
     #mode=1;#0-surge, 1-heave, 2-pitch
     
     
-    wec.buoy.Calculate(0,0,0,0);
+    wec.floater.Calculate(0,0,0,0);
         
             
     for idx,z in enumerate(zs):#calculate parameters for each submergence level
-        area[idx]=wec.buoy.Area(z);
-        vol[idx]=wec.buoy.Volume(z);
-        results=wec.buoy.Calculate(z,0,0,0)
-        results2=wec.buoy.Calculate(z-delta,0,0,0);
+        [area[idx],vol[idx]]=wec.area_vol(z);
+        
+        results=wec.floater.Calculate(z,0,0,0)
+        results2=wec.floater.Calculate(z-delta,0,0,0);
         for mode in [0,1,2]:
-            Fstat[mode][idx]=(results[0][mode]-results2[0][mode])*1/delta;
+            Fstat[mode][idx]=-1*(results[0][mode]-results2[0][mode])*1/delta;
             Fdyn[mode][idx]=np.real(results[1][mode]);
             Frad[mode][idx]=np.real(results[2][mode][mode]);
             Amass[mode][idx]=np.imag(results[2][mode][mode]);
             Am8[mode][idx]=results[3][mode];
             Fdiff[mode][idx]=np.imag(results[1][mode]);
-        floater_slider_spring[idx]=(wec.Calc_fs_spring(z,0)-wec.Calc_fs_spring(z-delta,0))*1/delta;
+        floater_slider_spring[idx]=wec.pto_mdc(z)[-1];#(wec.Calc_fs_spring(z,0)-wec.Calc_fs_spring(z-delta,0))*1/delta;
         
         #Get linear parameter only at zero position
         if z==0:
             mdc[1]=Frad[1][idx][0];
-            mdc[2]=-Fstat[1][idx]#-floater_slider_spring[idx]);
+            mdc[2]=Fstat[1][idx]#-floater_slider_spring[idx]);
     
     print("calculation finished")
     omega2=[];
@@ -108,28 +169,25 @@ def run():
     power_s=np.linspace(p_max*0+100,p_max*1.2,n);
     for idx,s in enumerate(speed_s):
         for jdx,p in enumerate(power_s):
-            x=np.zeros(wec.states);
-            x[1]=s;
-            #print(1*wec.Calc(0,wave,x,-1*p/s,0,[1,1,1])[8]/(p))
-            eff[jdx][idx]=1*wec.Calc(0,wave,x,-1*p/s,0,[1,1,1])[8]/(p);
-            #print(eff[jdx][idx])
+            #x=np.zeros(wec.states);
+            #x[1]=s;
+            #eff[jdx][idx]=1*wec.Calc(0,wave,x,-1*p/s,0,[1,1,1])[8]/(p);
+            eff[jdx][idx]=wec.eff_generator(s,p/s);
             
-            
-    print(eff)
     axis2=[];
     for s in speed_s:
         axis2.append("velocity="+str(s.round(2))+"m/s");
     pandas.DataFrame(np.vstack(((power_s/1000).round(2),eff.round(2).transpose())).transpose(),columns=["Generator_efficency_over_Power[kW]"]+axis2).to_csv(folder0+"gen_eff.csv",index=False)
     
-    mdc=mdc+wec.pto_mdc();#Get lienarized data from WEC
+    mdc=mdc+wec.pto_mdc(0);#Get lienarized data from WEC
 
     #... now the linearized eigenfrequencys can be calculated
     deigfreq=2*np.pi/np.sqrt(mdc[2]/mdc[0]-(0.5*mdc[1]/mdc[0])**2)
     eigfreq=2*np.pi/np.sqrt(mdc[2]/mdc[0])
-    pandas.DataFrame(np.vstack((mdc[0],mdc[1],mdc[2],eigfreq,deigfreq,wec.buoy.Calc_CoG())).transpose(),columns=["mass[kg]","damping [Ns/m]","stiffness [N/m]","eigenperiod [s]","damped eigenperiod [s]","center of gravity (heave) [m]"]).round(2).to_csv(folder0+"info.csv",index=False)
+    pandas.DataFrame(np.vstack((mdc[0],mdc[1],mdc[2],eigfreq,deigfreq,wec.floater.Calc_CoG())).transpose(),columns=["mass[kg]","damping [Ns/m]","stiffness [N/m]","eigenperiod [s]","damped eigenperiod [s]","center of gravity (heave) [m]"]).round(2).to_csv(folder0+"info.csv",index=False)
 
-    #clearning up
-    wec.release();
+    ##clearning up
+    #wec.release();
 
 if __name__ == "__main__":
     run();    
--- COERbuoy/utils.py
+++ COERbuoy/utils.py
@@ -8,30 +8,138 @@ Created on Fri Jun 25 17:16:12 2021
 
 import json;
 import os;
+import subprocess;
+import importlib;
+import numpy as np;
+
+global pkg_dir;
+pkg_dir=os.path.dirname(__file__);
 
 def get():
-    global pkg_dir;
-    pkg_dir=os.path.dirname(__file__);
+    global pkg_dir;#Package location
+
+    global cmddict;#Program <-> filename
+    cmddict={};
+
     
-    fp=os.path.join(pkg_dir,"settings.txt");
-    print(os.getcwd())
+    #read which command to use for which file type
+    try:
+        with open(os.path.join(pkg_dir,"stdcmds.txt")) as file:
+            cmddict=json.load(file);
+    except (FileNotFoundError):
+        get_command_for_extension();
+        
+        
     
+    def read_settings(fp): #read settings from file
+        with open(fp) as file:
+            data=json.load(file);
+            print(data)
+            global class_hydro, wec_dir, conn_ip, conn_port, resolution, dt_controller, msg_status, user_dir;
+            class_hydro=data.get("hydro",class_hydro);
+            wec_dir=data.get("WECfolder",wec_dir);
+            conn_ip=data.get("conn_ip",conn_ip);
+            conn_port=data.get("conn_port",conn_port);
+            user_dir=data.get("ctrl_path",user_dir);
+            resolution=data.get("resolution",resolution);
+            dt_controller=data.get("dt_controller",dt_controller);
+            msg_status=data.get("status_message",msg_status);
+         
+    #set default values
+    global class_hydro, wec_dir, conn_ip, conn_port, resolution, dt_controller, msg_status, controller0, results0, user_dir;
+    user_dir=os.path.join(os.path.expanduser("~"),"COERbuoy_data")
+    class_hydro="floater_bem";
+    wec_dir="[coerbuoy.data.coerbuoy]";
+    conn_ip="localhost";
+    conn_port=5050;
+    resolution=0.1;
+    dt_controller=0.1;
+    msg_status=[1,1,1,1,1,1,1,1,1,1];
+    
+    #read settings from package dir
+    read_settings(os.path.join(pkg_dir,"settings.txt"));
+    controller0=os.path.join(user_dir,"controller")
+    results0=os.path.join(user_dir,"results")
+    
+    #if available, read settings from working dir
     if os.path.exists("coerbuoy_settings.txt"):
-        fp="coerbuoy_settings.txt";
-    with open(fp) as file:
-        data=json.load(file);
-        print(data)
-        global class_hydro, wec_dir, conn_ip, conn_port, resolution, dt_controller, msg_status
-        class_hydro=data.get("hydro","Floater_BEM");
-        wec_dir=data.get("WECfolder","[COERbuoy.data.COERbuoy]");
-        conn_ip=data.get("conn_ip","localhost");
-        conn_port=data.get("conn_port",5050);
-        resolution=data.get("resolution",0.1);
-        dt_controller=data.get("dt_controller",0.1);
-        msg_status=data.get("status_message",[1,1,1,1,1,1,1,1,1,1]);
-     
-    if wec_dir[0] == '[' and wec_dir[-1]==']':
-        wec_dir=wec_dir[1:-1].replace('.','/');
-        wec_dir=os.path.join(pkg_dir,wec_dir);
-    a=0;
-get();
+        read_settings("coerbuoy_settings.txt");
+        
+    #transfer in WEC dir into absolute path, if necessary
+    wec_dir=WECpath(wec_dir);
+    
+def WECpath(wecdir):
+    #convention: [...] indicates folder in package dir
+    if wecdir[0] == '[' and wecdir[-1]==']':
+        wecdir=wecdir[1:-1].replace('.','/');
+        wecdir=os.path.join(pkg_dir,wecdir);
+    return wecdir;
+
+        
+#set a key value pair in settings
+def set_settings(key,value):
+        
+    with open(pkg_dir+"/settings.txt",'r') as jfile:
+        sets=json.load(jfile);
+        sets[key]=value;
+        with open(pkg_dir+"/settings.txt",'w') as jfile:
+            jfile.write(json.dumps(sets));
+
+def get_controller(ctrl_name=None):
+    
+    if ctrl_name!=None:
+        for path in ["",controller0]:
+            try:
+                l=os.listdir(path);
+                if ctrl_name in l:
+                    return os.path.join(path,ctrl_name);
+            except(FileNotFoundError):
+                    ...
+        raise Exception("ctrl file not found!");
+
+    else:        
+        lctrl=["linear","TCP"];
+        for path in [controller0]:
+            try:
+                l=os.listdir(path);
+                lctrl.append(l);
+            except(FileNotFoundError):
+                os.makedirs(controller0);
+                os.makedirs(results0);
+        return lctrl;
+
+    
+def get_command_for_extension():
+    
+    #Obtain commands
+    
+    #test python cmd
+    file="div/python.py"
+    for cmd in ["py","python3","python"]:
+        try:
+            p=subprocess.Popen([cmd,os.path.join(pkg_dir,file)])
+            if p.wait()==42:
+                #correct answer
+                cmddict["py"]=cmd;
+                break;
+        except:
+            ...
+            
+
+    #test octave cmd
+    file="div/octave.m"
+    for cmd in ["octave","matlab"]:
+        try:
+            p=subprocess.Popen([cmd,os.path.join(pkg_dir,file)])
+            if p.wait()==42:
+                #correct answer
+                cmddict["m"]=cmd;
+                break;
+        except:
+            ...
+            
+    
+    with open(os.path.join(pkg_dir,"stdcmds.txt"),'w') as file:
+        file.write(json.dumps(cmddict));
+    
+        
