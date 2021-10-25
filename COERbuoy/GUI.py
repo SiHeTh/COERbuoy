52e9706ef3e27e8269d06e6790ab9f2e07b2e2d1
Simon H. Thomas <simon.thomas.2021@mumail.ie>
2021-10-25 23:48:49 +0100

    new settings page (not fully functionally yet); read controller from home directory)

--- COERbuoy/GUI.py
+++ COERbuoy/GUI.py
@@ -16,16 +16,16 @@ import threading;
 import COERbuoy.Parameters as Parameters;
 import webbrowser;
 import COERbuoy.utils;
+from shutil import copyfile;
 #import svgwrite
 #from svgwrite import mm, deg
 #import concurrent.futures
 
-folder=COERbuoy.utils.pkg_dir+"/web/";
 def about():
     print("WEC Demonstrator, COER")
 
 fktdict={"regular_wave":reg_wave,
-         "brettschneider_wave":bretschneider_wave,
+         "bretschneider_wave":bretschneider_wave,
          "decay_test":decay_test}
 busy=False;
 
@@ -41,6 +41,7 @@ def threadfkt():
             jobs[idx]["power"]=fktdict[job["fkt"]](*job["args"])
             jobs[idx]["status"]="Done";
             print("Finished job "+job["name"])#+"; Absorbed: "+str(jobs[jobidx]["status"])+" %")
+            copyfile(os.path.join(COERbuoy.utils.pkg_dir,job["name"]),os.path.join(COERbuoy.utils.user_dir,job["name"]))
         idx=idx+1;
     busy=False;
     print("Work done!")
@@ -48,11 +49,6 @@ jobs=[];
 
 
         
-
-##Application is ready for multithreading; before max_worker is set to > 1,
-##please ensure the intferface simulation<>controller can handle multiple threads
-#pool = concurrent.futures.ThreadPoolExecutor(max_workers=4);
-#busy=False;
 class GUIServer(BaseHTTPRequestHandler):
     #t=threading.Thread(target=threadfkt,args=())    
     print(threading.active_count())
@@ -69,7 +65,7 @@ class GUIServer(BaseHTTPRequestHandler):
         f.close();
     
     def do_GET(self):
-        print(self.path)
+        #print(self.path)
         global busy;
         p=self.path.split("?");  
         if self.path[-3:]=="svg":
@@ -109,18 +105,26 @@ class GUIServer(BaseHTTPRequestHandler):
                 self.wfile.write(bytes(f.read(),"utf-8"))
                 f.close();
         if p[0][-3:]=="csv":
-            if os.path.isfile(COERbuoy.utils.pkg_dir+"/"+p[0][1:]):
+            if os.path.isfile(os.path.join(COERbuoy.utils.pkg_dir,p[0][1:])):
                 print("delivering "+p[0][1:])
-                f = open(COERbuoy.utils.pkg_dir+"/"+p[0][1:],'r')
+                f = open(os.path.join(COERbuoy.utils.pkg_dir,p[0][1:]),'r');
                 self.send_response(200)
                 self.send_header("Content-type","text/csv")
                 self.end_headers()
                 self.wfile.write(bytes(f.read(),"utf-8"))
                 f.close();
-        elif self.path=="csv":
-            if os.path.isfile(COERbuoy.utils.pkg_dir+"/"+self.path[1:]):
-                print("delivering "+self.path[1:])
-                f = open(COERbuoy.utils.pkg_dir+"/"+self.path[1:],'r')
+            #elif os.path.isfile(os.path.join(COERbuoy.utils.results0,p[0][1:])):
+             #   print("delivering "+self.path[1:])
+              #  f = open(os.path.join(COERbuoy.utils.results0,p[0][1:]),'r');
+               # self.send_response(200)
+                #self.send_header("Content-type","text/csv")
+                #self.end_headers()
+                #self.wfile.write(bytes(f.read(),"utf-8"))
+                #f.close();
+        elif self.path[-3:]=="csv":
+            if os.path.isfile(os.path.join(COERbuoy.utils.pkg_dir,self.path[1:])):
+                print("delivering "+self.path[1:]);
+                f = open(os.path.join(COERbuoy.utils.pkg_dir,self.path[1:]),'r');
                 self.send_response(200)
                 self.send_header("Content-type","text/csv")
                 self.end_headers()
@@ -155,9 +159,12 @@ class GUIServer(BaseHTTPRequestHandler):
         elif self.path=="/start.html":
             self.send_html_file("start.html");
             
+        elif self.path=="/settings.html":
+            self.send_html_file("settings.html");
+            
             
         elif self.path=="/jobs.json":
-            print("refreshing job list")
+            #print("refreshing job list")
             self.send_response(200)
             self.send_header("Content-type","text/json")
             self.end_headers()
@@ -173,7 +180,13 @@ class GUIServer(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes(f.read(),"utf-8"))
             f.close();
-
+        elif self.path=="/controllers.json":
+            print("delivering controller list")
+            self.send_response(200)
+            self.send_header("Content-type","text/json")
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps(COERbuoy.utils.get_controller()),"utf-8"));
+            
         elif self.path=="/settings.json":
             f = open(COERbuoy.utils.pkg_dir+"/settings.txt",'r')
             self.send_response(200)
@@ -203,29 +216,34 @@ class GUIServer(BaseHTTPRequestHandler):
     def do_POST(self):
         global busy;
         print(self.path)
+        
+        #set settings with key value pairs
         if self.path=="/set_settings.json":
             txt=self.rfile.read(int(self.headers["Content-Length"]))
-            print(txt)
             txt=json.loads(txt);
-            print(txt)
             with open(COERbuoy.utils.pkg_dir+"/settings.txt",'r') as jfile:
                 print(jfile);
                 sets=json.load(jfile);
-                print(sets);
             for k,v in txt.items():
                 sets[k]=v;
             Parameters.run();
             with open(COERbuoy.utils.pkg_dir+"/settings.txt",'w') as jfile:
                 jfile.write(json.dumps(sets));
-            #f.write(txt);
             self.send_response(200);
-            #self.send_header('Content-type','text/html');
-            #self.send_header('Location','/start.html');
             self.end_headers();
             COERbuoy.utils.get();
             Parameters.run();
+        
+        #replace settings file
+        if self.path=="/new_settings.json":
+            txt=self.rfile.read(int(self.headers["Content-Length"]))
+            with open(COERbuoy.utils.pkg_dir+"/settings.txt",'w') as jfile:
+                jfile.write(txt);
+            self.send_response(200);
+            self.end_headers();
+            COERbuoy.utils.get();
             
-            
+        #replace WEC parameters with new values
         if self.path=="/new_param.json":
             txt=self.rfile.read(int(self.headers["Content-Length"]))
             print("Write params to "+COERbuoy.utils.wec_dir+"/floater.txt.")
@@ -245,7 +263,7 @@ class GUIServer(BaseHTTPRequestHandler):
             if not busy:# not busy:
                 print(busy)
                 jobs.clear();
-                ##For security reasons only allow known command to be executed;
+                ##For security reasons only allow known commands to be executed;
                 ##can be removed in local installation
                 #if not (data1["ctrl"]=="python3 Controller_NL_TCP.py" 
                 #        or data1["ctrl"]=="python3 Controller1.py"
@@ -259,7 +277,7 @@ class GUIServer(BaseHTTPRequestHandler):
                             for e in data.items():
                                 strdata=strdata+e[0]+str(e[1])+"_";
                             strdata=strdata[:-1];
-                            name="results/"+strdata+data1["ctrl"].replace(' ','_')+".csv";
+                            name=os.path.join("results",strdata+data1["ctrl"].replace(' ','_')+".csv");
                             desc=strdata+". Using "+data1["ctrl"]+".";
                             print("Created job for "+desc+".")
                             if data["wave"]!="decay_test":
@@ -279,8 +297,18 @@ class GUIServer(BaseHTTPRequestHandler):
                 
     
 def run():
+    COERbuoy.utils.get();
     hostName="localhost"
     serverPort = 8080;
+    global folder;
+    folder=COERbuoy.utils.pkg_dir+"/web/";
+    
+    resdir=os.path.join(COERbuoy.utils.pkg_dir,"results");
+    reslist=os.listdir(resdir);
+    
+    for e in reslist:
+        if e[0]!=".":
+            os.remove(os.path.join(resdir,e));
    
     with open(COERbuoy.utils.pkg_dir+"/settings.txt") as file:
         data=json.load(file);
