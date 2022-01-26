512f8170da8cf70692d08f4c70715cd05c8f0637
Simon H. Thomas <simon.thomas.2021@mumail.ie>
2022-01-26 22:51:24 +0100

    small update for analyzer class

--- COERbuoy/analyzer.py
+++ COERbuoy/analyzer.py
@@ -27,7 +27,7 @@ def quartil (a, p):
     return b[np.argmax(np.cumsum(b)/aa>p)];
     
 class Analyzer():
-    d={"p":"wave period [s]","h":"wave height [m]",
+    d={"p":"wave period [s]","h":"wave height [m]","k_rel":"ka",
        "wave":"wave_type","RAO":"RAO","ctrl":"control method",
        "P":"Mean absorbed power [W]"};
     table=None;
@@ -36,7 +36,7 @@ class Analyzer():
         s1=np.argmax(data0[:1,:]>=0)
         return [data0[:,:s1],data0[:,s1:]];#return transient data - setady state data
                         
-    def read_folder(self,folder):#get a datatable from a folder
+    def read_folder(self,folder,a=1):#get a datatable from a folder
         self.table=None;
         for f in os.listdir(folder):#look for files in a folder
             if f[-4:] == ".csv":#check if csv
@@ -44,6 +44,11 @@ class Analyzer():
                 if len(info) == 8 and info[1]=="p" and info[3]=="h":#Check if it follows the naming convention
                     try:
                         data = self.read_file(os.path.join(folder,f))[1];
+                        
+                        per=float(info[2]);
+                        omega=6.28/per;
+                        k=omega**2/9.18;
+                        k_rel=k*a;
                         
                         row={"P":[(data[-1,-1]-data[-1,0])/(data[0,-1])],
                         "z075":[quartil(data[2,:],0.75)],
@@ -53,11 +58,14 @@ class Analyzer():
                         "dalpha075":[quartil(data[5,:],0.75)],
                         "RAO":[quartil(data[2,:],0.95)/float(info[4])],
                         "wtype":[info[0]],
-                        "p":[float(info[2])],
+                        "p":[per],
                         "h":[float(info[4])*2],
                         "wave":[info[0]],
                         "ctrl":[info[5]],
                         "WEC":[info[6]],
+                        "omega":[omega],
+                        "k":[k],
+                        "k_rel":[k_rel],
                         "model":[info[7]],
                         "data":[data]};
                              
@@ -76,14 +84,17 @@ class Analyzer():
         for s,e in zip(["p","h","ctrl","WEC","model"],[p,H,ctrl,WEC,model]):
             if e is not None:
                 if isinstance(e,float):
-                    t0=t0[(t0[s]>e-0.2) & (t0[s]<e+0.2)]; # bitwise & is equal to logical-and for booleans
+                    t0=t0[(t0[s]>e-0.4) & (t0[s]<e+0.4)]; # bitwise & is equal to logical-and for booleans
                 else:
                     t0=t0[t0[s]==e];
         #print(t0);
         return t0;
     def plot(self,xaxis,yaxis,group,t0,**kwargs):
         plt.figure();
-        c=['r','g','b','y','k']*5;
+        if "colors" in kwargs:
+            c=kwargs["colors"];
+        else:
+            c=['r','g','b','y','k']*5;
         t0=t0.sort_values(by=[xaxis,group]);
         gps=t0[group].unique().tolist();
         for i,e in t0.iterrows():
@@ -96,8 +107,17 @@ class Analyzer():
         #plt.legend(t0[group])
         plt.show()
     def plot_time(self,i,name,t0,**kwargs):
-        plt.figure();
-        c=['r','g','b','y','k']*5;
+        t0=t0.sort_values(by=["model"]);
+        if "axes" in kwargs:
+            axes=kwargs["axes"]
+        else:
+            #plt.figure()
+            fig,axes=plt.subplots(1,1)
+        if "colors" in kwargs:
+            c=kwargs["colors"];
+        else:
+            c=['r','g','b','y','k']*5;
+        
         k=0;
         for j,t in t0.iterrows():
             s2=len(t.data[1,:])-1;
@@ -110,13 +130,17 @@ class Analyzer():
                 
                 
             if k==0:
-                plt.plot(t.data[0,s1:s2],t.data[1,s1:s2],"k--");
-            plt.plot(t.data[0,s1:s2],t.data[i,s1:s2],c[k],linewidth=(len(t0)-k)*1.25+0.5);
+                axes.plot(t.data[0,s1:s2],t.data[1,s1:s2],"k--");
+            value0=0;
+            if i>6:
+                value0=t.data[i,s1];
+            axes.plot(t.data[0,s1:s2],t.data[i,s1:s2]-value0,c[k],linewidth=(len(t0)-k)*1.25+0.5);
             k=k+1;
-        plt.legend(["Wave"]+t0[name].tolist())
-        plt.xlabel("time [s]");
-        plt.title(kwargs.get("title",""));
-        plt.show()
+        axes.legend(["Wave"]+t0[name].tolist())
+        axes.set_xlabel("time [s]");
+        axes.set_title(kwargs.get("title",""));
+        #if not "axes" in kwargs:
+           # axes.show()
         
 
 if __name__=="__main__":
